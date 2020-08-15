41c19f96604c51fc5cde4dd565b01575110833c2
Yaremi Astorga <yare1894@gmail.com>
2020-08-15 14:01:33 -0600

    refactor: Add google font and theme provider

--- src/components/organisms/Shows/style.js
+++ src/components/organisms/Shows/style.js
@@ -1,5 +1,7 @@
 import styled from 'styled-components/macro'
 
 export const ShowsWrapper = styled.div`
+    padding: 10px;
     height: 100%;
+    font-family: 'Alata', sans-serif;
 `
---
+++ src/provider/themeProvider.js
@@ -0,0 +1,9 @@
+export default {
+    colors: {
+        primary: '#58846F',
+        secondary: '#E69670',
+        background: 'white',
+        containers: '#240F10',
+        borders: 'gray',
+    }
+}
