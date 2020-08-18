207219876f66cd3b80455f1d414c03d25344cdcd
Yaremi Astorga <yare1894@gmail.com>
2020-08-17 18:55:21 -0600

    feat: Order shows by name or score (asc y desc)

--- src/components/atoms/Select/index.js
+++ src/components/atoms/Select/index.js
@@ -1,21 +1,22 @@
-import React from 'react';
-import Text from '../Text';
-import { SelectWrapper, Selector } from './style';
+import React from 'react'
+import Text from '../Text'
+import { SelectWrapper, Selector } from './style'
 
 const Select = ({
     options,
     label,
+    onChange,
 }) => {
     return (
         <SelectWrapper>
             <Text type="paragraph">{label}</Text>
-            <Selector>
+            <Selector onChange={onChange}>
                 {options.map((option) => (
                     <option key={option.id} value={option.id}>{option.name}</option>
                 ))}
             </Selector>
         </SelectWrapper>
     )
-};
+}
 
-export default Select;
+export default Select
--- src/components/organisms/Shows/index.js
+++ src/components/organisms/Shows/index.js
@@ -1,8 +1,13 @@
-import React, { useState, useEffect, Suspense, lazy } from 'react'
+import React, {
+    useState,
+    useEffect,
+    Suspense,
+    lazy,
+    useCallback,
+} from 'react'
 import PropTypes from 'prop-types'
 import Select from '../../atoms/Select'
 import Page from '../../atoms/Page'
-// import ShowsList from '../../molecules/ShowsList'
 import Loading from '../../molecules/Loading'
 import useShows from '../../../hooks/useShows'
 import {
@@ -17,21 +22,34 @@ const headers = {
 }
 
 const searchOptions = [{
+    id: 'none', name: 'None'
+}, {
     id: 'ascName', name: 'Ascending name',
-},{
+}, {
     id: 'descName', name: 'Descending name',
-},{
+}, {
     id: 'ascScore', name: 'Ascending score',
-},{
+}, {
     id: 'descScore', name: 'Descending score',
 }]
 
 const ShowsList = lazy(() => import('../../molecules/ShowsList'))
 
+const orderShows = (orderBy, shows, order) => {
+    return shows.sort((showA, showB) => {
+        if (showA[orderBy] < showB[orderBy]) {
+            return order === 'asc' ? -1 : 1
+        }
+        if (showA[orderBy] > showB[orderBy]) {
+            return order === 'asc' ? 1 : -1
+        }
+        return 0
+    })
+}
+
 const Shows = ({ category, setTitle }) => {
     const [page, setPage] = useState(1)
     const { shows, isLoading } = useShows(category, page)
-
     // const isLoading = false
     // const shows = [{
     //     id: 1,
@@ -43,18 +61,54 @@ const Shows = ({ category, setTitle }) => {
     //     duration: 120,
     // }]
 
+    const [showsOrdered, setShowsOrdered] = useState([])
+
     useEffect(() => {
         setTitle(headers[category])
     }, [setTitle, category])
+
+    useEffect(() => setShowsOrdered(shows), [shows])
+
+    const handleOrderBy = useCallback((e) => {
+        let _showsOrdered = []
+        const _shows = [...shows]
+        switch (e.target.value) {
+            case 'ascName':
+                _showsOrdered = orderShows('name', _shows, 'asc')
+                break
+            case 'descName':
+                _showsOrdered = orderShows('name', _shows, 'desc')
+                break
+            case 'ascScore':
+                _showsOrdered = orderShows('vote_average', _shows, 'asc')
+                break
+            case 'descScore':
+                _showsOrdered = orderShows('vote_average', _shows, 'desc')
+                break
+            default:
+                _showsOrdered = shows
+                break
+        }
+        setShowsOrdered(_showsOrdered)
+    }, [shows])
 
     return (
         <Page>
             <ContentWrapper>
                 <SearchWrapper>
-                    <Select options={searchOptions} label="Order by" />
+                    <Select
+                        name="orderBy"
+                        options={searchOptions}
+                        label="Order by"
+                        onChange={handleOrderBy}
+                    />
                 </SearchWrapper>
                 <Suspense fallback={<Loading>Loading shows...</Loading>}>
-                    <ShowsList shows={shows} setPage={setPage} isLoading={isLoading} />
+                    <ShowsList
+                        shows={showsOrdered}
+                        setPage={setPage}
+                        isLoading={isLoading}
+                    />
                 </Suspense>
             </ContentWrapper>
         </Page>
